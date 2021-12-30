f8373b4eba2721d66c9109482f09f2029eece48d
Linchuan Chen <lionelchen@outlook.com>
2021-12-30 18:40:40 +0800

    add threesum

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static 双指针遍历.Solution.threeSum;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] arr = {0,0,0,0};
+	    System.out.println(threeSum(arr));
+    }
+
+
+
+}
---
+++ src/双指针遍历/Solution.java
@@ -0,0 +1,39 @@
+package 双指针遍历;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+    public static List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        for(int i = 0; i < nums.length; i++){
+            int j = i + 1;
+            int k = nums.length-1;
+            int sum = nums[i] * (-1);
+            if (i > 0 && nums[i]==nums[i-1]) continue;
+            while(j < k)
+                if(nums[j]+nums[k]==sum){
+                    //System.out.println("sum:"+sum);
+                    //System.out.println("i"+nums[i]+"j"+nums[j]+"k"+nums[k]);
+                    List<Integer> temp = new ArrayList<Integer>();
+                    temp.add(nums[i]);
+                    temp.add(nums[j]);
+                    temp.add(nums[k]);
+                    result.add(temp);
+                    while(j<k&&nums[j]==nums[j+1])
+                        j++;
+                    while(j<k&&nums[k] == nums[k-1])
+                        k--;
+                    j++;
+                    k--;
+                }else if(nums[j]+nums[k]<sum){
+                    j++;
+                }else{
+                    k--;
+                }
+        }
+        return result;
+    }
+}
